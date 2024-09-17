a8f28fcb3c2bf45d2a2560345ee2ca3e72b6a3b1
chajus1 <chajus1@googlemail.com>
2024-09-17 22:39:29 +0100

    Added class

---
+++ NewAbilityScoreCalculator/AbilityScoreCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewAbilityScoreCalculator
+{
+    internal class AbilityScoreCalculator
+    {
+        public int RollResult = 14;
+        public double DividedBy = 1.75;
+        public int AddAmount = 2;
+        public int Minimum = 3;
+        public int Score;
+
+        public void CalculateAbilityScore()
+        {
+            // Divide the roll result by the DividedBy field
+            double divided = RollResult / DividedBy;
+
+            // Add AddAmount to the result and round down
+            int added = AddAmount + (int)divided;
+
+            // If the result is too small, use Minimum
+            if (added < Minimum)
+                Score = Minimum;
+            else
+                Score = added;
+        }
+    }
+}
--- NewAbilityScoreCalculator/Program.cs
+++ NewAbilityScoreCalculator/Program.cs
@@ -6,10 +6,15 @@
         {
 
 
-            sbyte c = -125;
-            char b = 't';
-            Convert.ToByte("10111", 2);
-            Console.WriteLine($"{c} {b}");
+            int myInt = 10;
+            byte myByte = (byte)myInt;
+            double myDouble = myByte;
+            string myString = "false";
+            short myShort = (short)myInt;
+            char myChar = 'x';
+            myString = myString + myInt + myByte + myDouble + myShort + myChar;
+
+            Console.WriteLine(myString.GetType());
         }
     }
 }
